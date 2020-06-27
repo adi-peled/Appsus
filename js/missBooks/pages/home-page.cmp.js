ae0775ce93acee97661c10d4fe275bb0e81c9455
adi_peled <adipeled499@gmail.com>
2020-06-27 22:17:35 +0300

    3

--- js/missBooks/pages/home-page.cmp.js
+++ js/missBooks/pages/home-page.cmp.js
@@ -5,9 +5,9 @@ export default {
             <p class="apsus">APSUS</p>
            <p class="home-page-sentence"> Tough times never last but tough people do.</p>
             <div class="home-page-routes">
-               <router-link to="/note"><img   @click="updateIsHomePage" title="Note App" src="../img/note.png"></router-link>  
-               <router-link to="/book"><img   @click="updateIsHomePage"title="Book App" src="../img/book.png"> </router-link>  
-               <router-link to="/email/inbox"><img   @click="updateIsHomePage"title="Email App" src="../img/email.png"></router-link>  
+               <router-link to="/note"><img   @click="updateIsHomePage" title="Note App" src="../../img/note.png"></router-link>  
+               <router-link to="/book"><img   @click="updateIsHomePage"title="Book App" src="../../img/book.png"> </router-link>  
+               <router-link to="/email/inbox"><img   @click="updateIsHomePage"title="Email App" src="../../img/email.png"></router-link>  
                
 
             </div>
