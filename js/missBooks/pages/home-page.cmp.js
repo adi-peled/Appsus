d4793f0c019af00e5ba95b4746d6a9d9077a3635
Maor <maorba6@gmail.com>
2020-06-27 21:39:50 +0300

    FINALE

--- js/missBooks/pages/home-page.cmp.js
+++ js/missBooks/pages/home-page.cmp.js
@@ -1,9 +1,12 @@
 export default {
     template: `
     <section class="home-page">
+            <p class="apsus">APSUS</p>
            <p class="home-page-sentence"> Tough times never last but tough people do.</p>
-            <div>
-               <router-link to="/note"><img src="/img/note-route.jpg" alt=""></router-link>  
+            <div class="home-page-routes">
+               <router-link to="/note"><img title="Note App" src="/img/note.png"></router-link>  
+               <router-link to="/book"><img title="Book App" src="/img/book.png"> </router-link>  
+               <router-link to="/email"><img title="Email App" src="/img/email.png"></router-link>  
                
 
             </div>
@@ -14,7 +17,11 @@ export default {
 }
 
 
-
+{
+    /* <img src="/img/note.png">
+    <img src="/img/email.png">
+    <img src="/img/book.png"> */
+}
 
 /* <router-link to="/note">note </router-link> |
 <router-link to="/book"> book App </router-link> | 
